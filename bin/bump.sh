c4f4b58c9c5d5ee7caa9709391956cbbfdca4a4a
Telpenarmo <telpenarmo@protonmail.com>
2024-02-24 13:10:04 +0100

    bump to v1.1.2 (this time correctly)

--- bin/bump.sh
+++ bin/bump.sh
@@ -4,5 +4,6 @@ if [ -z "$1" ]; then
     echo "Usage: $0 <new_version>"
     exit 1
 fi
-sed -i "s/^version.*/version = \"$1\"/g" src-tauri/Cargo.toml
-sed -i "s/\"version\".*/\"version\": \"$1\",/g" package.json
+version=$(echo $1 | sed 's/^v//g')
+sed -i "s/^version.*/version = \"$version\"/g" src-tauri/Cargo.toml
+sed -i "s/\"version\".*/\"version\": \"$version\",/g" package.json
